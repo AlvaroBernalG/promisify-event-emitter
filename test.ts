54989690590e06d7e3f9c562f7342ee240ba419b
Alvaro Bernal <alvaro.bernal.gimeno@gmail.com>
2019-07-20 19:58:31 +0100

    Increase test coverage - Create mini test framework

--- test.ts
+++ test.ts
@@ -1,23 +1,40 @@
 import EventPromise, {Message, Callback} from './index';
 
+enum EXIT {
+  SUCESS,
+  FAIL
+}
+
 const log = console.log;
-const assert = console.assert;
+const assert = (result: boolean, ...title: any[]) => {
+  if (result === false) {
+    log('');
+    log('\x1b[31mFAILED: %s\x1b[0m', ...title);
+    log('');
+    process.exit(EXIT.FAIL);
+    return;
+  }
+  log('\x1b[32m%s\x1b[0m', ...title); 
+}
 
-const wait = (ms: number): Promise<void> => new Promise((resolve, reject) => setTimeout(resolve, ms));
+const wait = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+const sum = (a: number, b:number): number => a + b;
 
-let events = new EventPromise<string | void>();
-const logWorks = async (...works): Promise<string> => {
+let events = new EventPromise<string | void>(); 
+
+const countOne = async (): Promise<string> => {
   return 'one';
 }
 
-events.on('test', logWorks);
+// console.log(process.exit(1));
+events.on('counter', countOne);
 
-events.on('test', async (message: Message<string>): Promise<string> => {
+events.on('counter', async (_: Message<string>): Promise<string> => {
   await wait(300); 
   return 'two';
 });
 
-events.on('test', async () => 'three');
+events.on('counter', async () => 'three');
 
 let evn = new EventPromise<number>();
 
@@ -36,14 +53,40 @@ events.on('other', async () => {
   return 'test';
 });
 
+const numbers = new EventPromise<number>();
+
+numbers.on('sum', async (m: Message<number>) => {
+  return m.payload + 1;
+});
+
+numbers.on('sum', async (m: Message<number>) => {
+  return m.payload + 1;
+});
+
+let plusOne = async (m: Message<number>) => m.payload++
+
+numbers.on('sum', plusOne);
+
 (async function start() {
-  log('Starting Tests...');
-  const res = await events.emit('test', {payload: 'yeap'});
+
+  log('Starting test...');
+
+  assert(events.emit('counter') instanceof Promise, 'emit() should return a Promise.');
+
+  const res = await events.emit('counter', {payload: 'yeap'});
   assert(res.join(' ') === 'one two three', 'Result should have been one two three. Instead got [' +res.join(' ')+ ']');
   events.emit('other');
   const [doubleres] = await evn.emit('double', {payload: 2});
-  assert(doubleres === 4, ' Result of double should have been 4. Instead got ', doubleres);
-  evn.emit('double', {payload: 3}).then(e => log('The result of doublign 3 is ', e));
-  evn.emit('double', {payload: 4}).then(e => log('The result of doubling 4 is ', e));
+  assert(doubleres === 4, 'Result of double should have been 4. Instead got ', doubleres);
+
+  numbers.off('sum', plusOne);
+  let sumResult = await numbers.emit('sum', {payload: 0});
+  assert(sumResult.reduce(sum, 0) === 2, 'The sumResult should be [1]. Instead got '+ sumResult);
+
+  // Should be able to remove events.
+  numbers.off('sum')
+  sumResult = await numbers.emit('sum', {payload: 0});
+  assert(sumResult === undefined, 'The sumResult should be undefined. Instead got '+ sumResult);
+
 })();
 
