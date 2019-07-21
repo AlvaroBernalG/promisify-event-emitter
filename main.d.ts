fa7c4dc03e2835f1b4cb1fa19a3dd797be0f1f58
Alvaro Bernal <alvaro.bernal.gimeno@gmail.com>
2019-07-21 15:45:38 +0100

    Create type declaration

---
+++ main.d.ts
@@ -0,0 +1,26 @@
+
+declare interface Message<T> {
+    payload: T
+}
+
+declare type Callback<T> = (emitterMessage: Message<T>) => Promise<T>;
+
+declare class  EventEmitterPromisified<A> {
+
+    on(eventName: string, eventCallback: Callback<A>): EventEmitterPromisified<A>;
+
+    prepend(eventName: string, eventCallback: Callback<A>) :EventEmitterPromisified<A>;
+
+    emit(eventName: string, message?: Message<A>): Promise<Array<A>>; 
+
+    off(eventName: string, eventCallback?: Callback<A>): EventEmitterPromisified<A>;
+
+    listeners(eventName: string): Array<Callback<A>>;
+    
+    eventNames(): Array<string>;
+
+    once(eventName: string, eventCallback: Callback<A>): EventEmitterPromisified<A>;
+
+    times(eventName, eventCallback: Callback<A>, times: number): EventEmitterPromisified<A>;
+}
+
